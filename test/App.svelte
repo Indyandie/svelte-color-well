cd1db1cd6275291caa8fbd6f211980e9707764d0
Andy Contreras <andy.j.contreras@me.com>
2020-05-25 21:42:17 -0500

    testing reactivity

--- test/App.svelte
+++ test/App.svelte
@@ -1,8 +1,9 @@
 <script>
     import Colorwell from '../src/Colorwell'
-    let myColor
+    $: myColor = '#FF0000'
 </script>
 
 <h1>{myColor}</h1>
+<input type="text" bind:value={myColor}>
 
 <Colorwell bind:colorGlob={myColor} />
