fba6e647cc7c395117b63acdf2883c34b2e4d1dc
Andy Contreras <andy.j.contreras@me.com>
2020-05-27 23:12:22 -0500

    hav >> hsl and tidy

--- src/Colorwell.svelte
+++ src/Colorwell.svelte
@@ -13,17 +13,29 @@
 
     $: hue = 0
     $: saturation = 100
-    $: value = 10
-    $: valueRange = chrm.hsv(hue, (saturation * 0.01), (1)).hex().toUpperCase()
-    $: satRange = chrm.hsv(hue, (1), (value * 0.01)).hex().toUpperCase()
-    $: rangeArr = [ valueRange, satRange ]
+    $: light = 10
+
+    $: lightRangeCenter     = chrm.hsl(hue, (saturation * 0.01), (.5))
+    $: satRangeStart        = chrm.hsl(hue, (0), (light * 0.01))
+    $: satRangeEnd          = chrm.hsl(hue, (1), (light * 0.01))
+    $: hueRange             = chrm.hsl(hue, 1, 0.5)
+    $: rangeArr = {
+        ltCen: lightRangeCenter, 
+        stRang: [
+            satRangeStart, 
+            satRangeEnd,
+        ], 
+        hue: hueRange,
+        color: color
+    } 
+        
 
     $: color = chrm.random()
     
     $: colorContrast = chrm.contrast('white', color) < 4.5 
 
     function updateColor() {
-        color = chrm.hsv(hue, (saturation * 0.01), (value * 0.01)).hex().toUpperCase()
+        color = chrm.hsl(hue, (saturation * 0.01), (light * 0.01)).hex().toUpperCase()
         updateColorGlob()
     }
 
@@ -31,21 +43,21 @@
         colorGlob = color
     }
 
-    function updateColorHSV() {
-        let colorHSV = chrm(color).hsv()
+    function updateColorHSL() {
+        let colorHSL = chrm(color).hsl()
 
         if (hue === NaN) {
             hue = hue
         } else {
-            hue = Math.floor(colorHSV[0])   
+            hue = Math.floor(colorHSL[0])   
         }
-        saturation = Math.floor(colorHSV[1]*100)
-        value = Math.floor(colorHSV[2]*100)
+        saturation = Math.floor(colorHSL[1]*100)
+        light = Math.floor(colorHSL[2]*100)
     }
 
     function updateColorToGlob() {
         color = colorGlob
-        updateColorHSV()
+        updateColorHSL()
     }
 
 
@@ -69,7 +81,7 @@
     class={`colorwell-selector ${colorContrast ? 'black': '' }`} 
     style={`background: ${color}`} 
     on:click={e => {
-        updateColorHSV()
+        updateColorHSL()
         showModal = !showModal
     }}
 >
@@ -80,7 +92,6 @@
     <div class={`colorwell-modal ${showModal ? 'hidden': ''}`} >
 
         <div 
-            
             class="colorPal" 
             style={`background: ${color}`}
             on:click="{e => {
@@ -88,12 +99,14 @@
                 showModal = true
             }}"
         >
-            <strong class:black={colorContrast} >{color}</strong>
+            <strong class:black={colorContrast} >
+                <code>{color}</code>
+            </strong>
         </div>
 
         <RangeInput bind:hueColor={rangeArr} hue={true} on:change={updateColor} bind:value={hue} label="Hue" MAX={360}/>
         <RangeInput bind:hueColor={rangeArr} sat={true} on:change={updateColor} bind:value={saturation} label="Saturation" MAX={100}/>
-        <RangeInput bind:hueColor={rangeArr} light={true} on:change={updateColor} bind:value={value} label="Value" MAX={100}/>
+        <RangeInput bind:hueColor={rangeArr} light={true} on:change={updateColor} bind:value={light} label="light" MAX={100}/>
         
     </div>
 {/if}
@@ -110,8 +123,8 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        color: white;
-        border: 2px solid #333;
+        color: rgba(255, 255, 255, .8);
+        border: 2px solid rgb(51, 51, 51, .25);
         cursor: pointer;
     }
 
@@ -122,7 +135,7 @@
         border-radius: 16px;
         display: flex;
         flex-direction: column;
-        border: 2px solid #aaa; 
+        border: 2px solid rgb(170, 170, 170, .5); 
         background: white;
         z-index: 90000;
         position: absolute;
@@ -137,22 +150,24 @@
         min-height: 32px;
         margin-bottom: 32px;
         color: white;
+        border: 2px solid rgb(0, 0, 0, .1);
         display: flex;
         justify-content: center;
         align-items: center;
         border-radius: 8px;
-        font-size: 1.25em; 
+        font-size: 1.5em; 
         transition: all 200ms ease-out;
         cursor: pointer;
+        text-transform: uppercase;
     }
 
     .colorPal:hover {
-        border: 2px solid black;
+        border: 2px solid rgb(0, 0, 0, .25);
         transform: scaleY(1.15)
     }
 
     .black {
-        color: black;
+        color: rgb(0, 0, 0, .8);
     }
 
     .hidden {
