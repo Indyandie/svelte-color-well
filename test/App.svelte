758d93def5b3a4a05398d8df0a47882591d0fbe3
Andy Contreras <andy.j.contreras@me.com>
2020-10-18 17:10:46 -0500

    new test

--- test/App.svelte
+++ test/App.svelte
@@ -1,22 +1,5 @@
 <script>
-  import Colorwell from '../src/Colorwell'
-  import chrm from 'chroma-js'
-
-  let myColor = '#FF0000'
-
-  function randBaseColor () {
-      myColor = chrm.random()
-  }
-
-  document.addEventListener('keydown', event => {
-      if (event.code === 'Space') {
-          randBaseColor()
-      }
-  })
+  
 </script>
 
-<h1>{myColor}</h1>
-<input type="text" bind:value={myColor}>
-
-<Colorwell bind:colorGlob={myColor} />
-<Colorwell />
+<h1>Testing</h1>
