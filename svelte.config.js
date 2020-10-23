a848ff1a8e8af128ca8859737d009568a87113ce
Andy Contreras <andy.j.contreras@me.com>
2020-10-23 01:58:11 -0500

    failed attempt not supported

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+// const preProcessor = require('svelte-preprocess')
+
+// module.exports = {
+//   preprocess: preProcessor()
+// }
+
+// /* import preProcessor from 'svelte-preprocess'
+
+// export default {
+//   preprocess: preProcessor()
+// } */
