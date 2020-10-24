6052a5a6ad1511f5296b3b1a48a0ec07b4c7bd3c
Andy Contreras <andy.j.contreras@me.com>
2020-10-23 23:20:16 -0500

    color well

---
+++ src/Components/Well.svelte
@@ -0,0 +1,15 @@
+<script>
+  import '../styles/well.less'
+
+  export let color = '#2DB7F2'
+  export let colorContrast = false
+</script>
+
+<div
+  class="colorPal"
+  style={ `background: ${color}` }
+>
+  <strong class:black={colorContrast} >
+    <code>{ color.toUpperCase() }</code>
+  </strong>
+</div>
