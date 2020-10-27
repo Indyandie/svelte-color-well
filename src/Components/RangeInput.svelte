123611c6b4c90fb8603e86d1f7d94a5c089ffe9c
Andy Contreras <andy.j.contreras@me.com>
2020-10-27 00:51:02 -0500

    chnage event and sel focus

--- src/Components/RangeInput.svelte
+++ src/Components/RangeInput.svelte
@@ -1,11 +1,13 @@
 <script>
   import '../styles/inputrange.less'
+
   export let value = 0
   export let id = 'range'
   export let min = 0
   export let max = 100
   export let labelName = 'Label'
   export let colorSel = 'white'
+  export let colorSelFocus = 'white'
   export let bgGrad
   export let extraStyles = ''
 </script>
@@ -20,6 +22,7 @@
   {max}
   {min}
   {id}
+  on:change
   bind:value
-  style="--thumb-sel: {colorSel}; --bg-gradient: {bgGrad}; {extraStyles}"
+  style="--thumb-sel: {colorSel}; --thumb-sel-focus: {colorSelFocus}; --bg-gradient: {bgGrad}; {extraStyles}"
 >
